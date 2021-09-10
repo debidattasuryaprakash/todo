57e0b4e3f7deb968de95072868cec019756b70da
debidattasuryaprakash <suryaprakash.d20@gmail.com>
2021-09-10 11:51:13 +0530

    wrong

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/api/tasks", tasks);
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*",(req, res)=> {
-    res.sendFile(path.join(__dirname,"build,"index.html"))
+    res.sendFile(path.join(__dirname,"build","index.html"))
 });
 
 const port = process.env.PORT || 8080;
